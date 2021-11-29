e536097f9cc45e68c03de66365d96f39d35ef08a
sinseungmi <tlstmdal3@naver.com>
2021-11-29 23:51:41 +0900

    detail

--- Market_Brokurly/src/main/java/com/teamright/brokurly/product/controller/ProductDetailController.java
+++ Market_Brokurly/src/main/java/com/teamright/brokurly/product/controller/ProductDetailController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
